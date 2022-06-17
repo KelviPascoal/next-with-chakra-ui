be58bfae42667df4f24832a13b1ab3b9973f0e59
kelvi <kelvi_ugo@hotmail.com>
2022-06-16 22:25:21 -0300

    feat: add hrefs to browse pages

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,21 +1,25 @@
 import {
   Icon,
-  Link,
+  Link as ChakraLink,
   Text,
   LinkProps as ChakraLinkProps,
 } from "@chakra-ui/react";
 import { ElementType } from "react";
+import { ActiveLink } from "../ActiveLink";
 
 type NavLinkProps = ChakraLinkProps & {
   icon: ElementType;
   children: string;
+  href: string;
 };
 
-export const NavLink = ({ children, icon, ...props }: NavLinkProps) => (
-  <Link display="flex" alignItems="center" {...props}>
-    <Icon as={icon} />
-    <Text ml="4" fontWeight="medium">
-      {children}
-    </Text>
-  </Link>
+export const NavLink = ({ children, icon, href, ...props }: NavLinkProps) => (
+  <ActiveLink href={href}>
+    <ChakraLink display="flex" alignItems="center" {...props}>
+      <Icon as={icon} />
+      <Text ml="4" fontWeight="medium">
+        {children}
+      </Text>
+    </ChakraLink>
+  </ActiveLink>
 );
